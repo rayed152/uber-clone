08bee70abad23ef6e0647eff578210a68bba80b7
rayed152 <rayed152@gmail.com>
2024-09-11 02:27:40 +0600

    Done connecting clerk.

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,10 +1,22 @@
+import { SignedIn, SignedOut, useUser } from "@clerk/clerk-expo";
+import { Link } from "expo-router";
 import { View, Text } from "react-native";
-import React from "react";
 
 export default function Home() {
+  const { user } = useUser();
   return (
     <View>
-      <Text>home</Text>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+      <SignedOut>
+        <Link href="/sign-in">
+          <Text>Sign In</Text>
+        </Link>
+        <Link href="/sign-up">
+          <Text>Sign Up</Text>
+        </Link>
+      </SignedOut>
     </View>
   );
 }
